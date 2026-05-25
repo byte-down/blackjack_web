22e1a65157911da1f66e3cd6facac2981e959bf9
gv-can-code <georgevarahidis@gmail.com>
2026-05-25 03:19:52 -0400

    Automated web deployment update

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3877838171"
+    serviceWorkerVersion: "2971530512"
   }
 });
